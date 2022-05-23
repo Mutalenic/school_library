f95173a1a39d05f0f2100b3ef31c337c693d2408
Nicholas Mutale <nicomutale@gmail.com>
2022-05-23 15:43:33 +0200

    Add Getters Setters

--- person_class.rb
+++ person_class.rb
@@ -1,4 +1,6 @@
 class person
+    attr_accessor :name, :age
+    attr_reader  :id
     def initialize(name = 'Unknown', age, parent_permission:true) 
     @id = Random.rand(1...1000)
     @name = name
