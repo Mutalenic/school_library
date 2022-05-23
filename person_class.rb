f17634302b8873295d40ec6d1bd168fdafce84e4
Nicholas Mutale <nicomutale@gmail.com>
2022-05-23 15:33:44 +0200

    Create class person Add instance variables constructor

---
+++ person_class.rb
@@ -0,0 +1,7 @@
+class person
+    def initialize(name = 'Unknown', age, parent_permission:true) 
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    end
