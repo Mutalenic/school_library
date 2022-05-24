9613c8bad1f40d9032ad541a1679f1bc0e3e2854
Nicholas Mutale <nicomutale@gmail.com>
2022-05-24 12:13:28 +0200

    Add Decorator class

---
+++ decorator.rb
@@ -0,0 +1,13 @@
+require_relative './nameable_class'
+
+class Decorator < Nameable
+ def initialize(nameable_object)
+    @nameable_object = nameable_object
+ super()
+ end
+
+ def correct_name
+    @nameable_object.correct_name
+ end
+end
+
