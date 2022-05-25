8217bff8b3f87ee5ed6e7d9f7e3a87437c8b3830
Nicholas Mutale <nicomutale@gmail.com>
2022-05-25 09:54:08 +0200

    Add Rental method to person class

--- person_class.rb
+++ person_class.rb
@@ -1,13 +1,14 @@
 require_relative './nameable_class'
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
@@ -17,6 +18,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rental << rental
   end
 
   private
