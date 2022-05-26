b01c98fb00881def4fad1793be9e656568770d18
Nicholas Mutale <nicomutale@gmail.com>
2022-05-26 11:46:32 +0200

    List all books

--- app.rb
+++ app.rb
@@ -6,4 +6,14 @@ class App
         @books = []
         @people = []
     end
+
+    def list_books
+        if @books.empty?
+            puts "No books in the library"
+        else
+        @books.each_with_index do |book, index|
+            puts "#{index + 1}) Title: \"#{book.title}\"|Author: by #{book.author}"
+        end
 end
+
+
