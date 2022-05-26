1da93c121a30158e01880b5c89b23c435596a32f
Nicholas Mutale <nicomutale@gmail.com>
2022-05-26 12:00:41 +0200

    List all people

--- app.rb
+++ app.rb
@@ -14,6 +14,16 @@ class App
         @books.each_with_index do |book, index|
             puts "#{index + 1}) Title: \"#{book.title}\"|Author: by #{book.author}"
         end
+    end
+
+    def list_people
+        if @people.empty?
+            puts 'No people added'
+        else
+            @people.each_with_index do |person, index|
+                puts "#{index + 1}) [#{person.class.name}] Name: #{person.name}|Age: #{person.age} |ID: #{person.id}"
+            end
+        end
 end
 
 
