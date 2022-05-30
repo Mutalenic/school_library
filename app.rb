9c2fac06c26937b35338d4a39ef4c7d9dfbb2937
Nicholas Mutale <nicomutale@gmail.com>
2022-05-30 21:21:44 +0200

    Update books

--- app.rb
+++ app.rb
@@ -53,15 +53,7 @@ class App
     end
   end
 
-  def list_books
-    if @books.empty?
-      puts 'No books in the library'
-    else
-      @books.each_with_index do |book, index|
-        puts "#{index + 1}) Title: \"#{book.title}\"|Author: by #{book.author}"
-      end
-    end
-  end
+  
 
   
   
--- books.rb
+++ books.rb
@@ -5,4 +5,14 @@ class BOOKS
     def initialize
         @books = []
     end
+    
+    def list_books
+        if @books.empty?
+          puts 'No books in the library'
+        else
+          @books.each_with_index do |book, index|
+            puts "#{index + 1}) Title: \"#{book.title}\"|Author: by #{book.author}"
+          end
+        end
+      end
     end
